5f9ee2a35322a93e3941e51869850252f49081c4
门殿春 <190636148@qq.com>
2017-06-26 17:14:30 +0800

    上传图片接口支持上传患者检查照片

--- api/config/params.php
+++ api/config/params.php
@@ -3,7 +3,7 @@ return [
     'adminEmail' => 'admin@example.com',
     // token 有效期默认1天
     'user.apiTokenExpire' => 1*24*3600,
-    'upload.type' => ['avatar','clinic'],
+    'upload.type' => ['avatar','clinic','patient'],
     'upload.imageType' => ['jpg','jpeg','gif','png'],
     'upload.maxsize' => '5', //MB
     'list.pagesize' => 10,
