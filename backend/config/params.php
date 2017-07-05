b4890d49418e77296ee1ef28c1e913f3a85b99bb
chenyawei <chenyawei@foxmail.com>
2017-07-05 10:00:23 +0800

    修改图片上传路径

--- backend/config/params.php
+++ backend/config/params.php
@@ -3,7 +3,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'upload.maxsize' => '20', //MB
 	// 图片服务器的域名设置，拼接保存在数据库中的相对地址，可通过web进行展示
-	'domain' => 'http://'.$_SERVER['HTTP_HOST'].'/',
+	'domain' => 'http://img.hdyzs.com/',
 	'webuploader' => [
 		// 后端处理图片的地址，value 是相对的地址
 		'uploadUrl' => 'expert/upload',
@@ -23,6 +23,6 @@ return [
 			],
 		],
 	],
-	'imageUploadRelativePath' => './uploads/', // 图片默认上传的目录
-	'imageUploadSuccessPath' => 'uploads/', // 图片上传成功后，路径前缀
+	'imageUploadRelativePath' => '../../data/img/uploads/experts', // 图片默认上传的目录
+	'imageUploadSuccessPath' => 'uploads/experts', // 图片上传成功后，路径前缀
 ];
