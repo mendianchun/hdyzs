70ce571e0549e42c47ebe431575d50e5fc10a406
门殿春 <190636148@qq.com>
2017-07-12 11:57:09 +0800

    apponitmentSearch

--- common/models/AppointmentSearch.php
+++ common/models/AppointmentSearch.php
@@ -12,10 +12,6 @@ use common\models\Appointment;
  */
 class AppointmentSearch extends Appointment
 {
-	public $start_time;
-	public $end_time;
-	public $real_end_time;
-
 	public function attributes()
     {
         return array_merge(parent::attributes(),['clinicName','expertName']);
@@ -27,7 +23,7 @@ class AppointmentSearch extends Appointment
     {
         return [
             [['appointment_no', 'order_starttime', 'order_endtime', 'order_fee', 'real_starttime', 'real_endtime', 'real_fee', 'patient_age', 'pay_type', 'status', 'pay_status', 'is_sms_notify', 'fee_type', 'created_at', 'updated_at'], 'integer'],
-            [['clinic_uuid', 'expert_uuid', 'patient_name', 'patient_mobile', 'patient_idcard', 'patient_description', 'expert_diagnosis', 'clinicName', 'expertName','start_time','end_time'], 'safe'],
+            [['clinic_uuid', 'expert_uuid', 'patient_name', 'patient_mobile', 'patient_idcard', 'patient_description', 'expert_diagnosis', 'clinicName', 'expertName'], 'safe'],
         ];
     }
 
@@ -75,11 +71,11 @@ class AppointmentSearch extends Appointment
         // grid filtering conditions
         $query->andFilterWhere([
             'appointment_no' => $this->appointment_no,
-            'order_starttime' => $this->order_starttime,
-            'order_endtime' => $this->order_endtime,
+//            'order_starttime' => $this->order_starttime,
+//            'order_endtime' => $this->order_endtime,
             'order_fee' => $this->order_fee,
-            'real_starttime' => $this->real_starttime,
-            'real_endtime' => $this->real_endtime,
+//            'real_starttime' => $this->real_starttime,
+//            'real_endtime' => $this->real_endtime,
             'real_fee' => $this->real_fee,
             'patient_age' => $this->patient_age,
             'pay_type' => $this->pay_type,
@@ -89,20 +85,24 @@ class AppointmentSearch extends Appointment
             'fee_type' => $this->fee_type,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            'clinic_uuid' => $this->clinic_uuid,
         ]);
 
-        $query->andFilterWhere(['like', 'clinic_uuid', $this->clinic_uuid])
-            ->andFilterWhere(['like', 'expert_uuid', $this->expert_uuid])
+        $query->andFilterWhere(['like', 'expert_uuid', $this->expert_uuid])
             ->andFilterWhere(['like', 'patient_name', $this->patient_name])
             ->andFilterWhere(['like', 'patient_mobile', $this->patient_mobile])
             ->andFilterWhere(['like', 'patient_idcard', $this->patient_idcard])
             ->andFilterWhere(['like', 'patient_description', $this->patient_description])
             ->andFilterWhere(['like', 'expert_diagnosis', $this->expert_diagnosis]);
 
-	    $query->andFilterWhere(['>','order_starttime',$this->start_time]);
-	    $query->andFilterWhere(['<','order_starttime',$this->end_time]);
+        $query->andFilterWhere(['>=','order_starttime',$this->order_starttime]);
+        $query->andFilterWhere(['<','order_starttime',$this->order_endtime]);
 
-	    $query->andFilterWhere(['>','real_endtime',$this->real_end_time]);
+        if($this->real_endtime == 0){
+            $query->andFilterWhere(['>','real_endtime',$this->real_endtime]);
+        }else{
+            $query->andFilterWhere(['<','real_endtime',$this->real_endtime]);
+        }
 
         $query->join('INNER JOIN','clinic','appointment.clinic_uuid = clinic.user_uuid');
         $query->andFilterWhere(['like','clinic.name',$this->clinicName]);
