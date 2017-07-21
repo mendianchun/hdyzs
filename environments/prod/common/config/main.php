109e8d3076a28f6795b1327c7460c0a0b340173c
门殿春 <190636148@qq.com>
2017-07-21 09:32:54 +0800

    生产环境开启scheme缓存

--- environments/prod/common/config/main.php
+++ environments/prod/common/config/main.php
@@ -14,6 +14,14 @@ return [
             'username' => 'handian',
             'password' => 'cu7CheW4',
             'charset' => 'utf8',
+
+            'enableSchemaCache' => true,
+
+            // Duration of schema cache.
+            'schemaCacheDuration' => 3600,
+
+            // Name of the cache component used to store schema information
+            'schemaCache' => 'cache',
         ],
     ],
     'timeZone'=>'Asia/Shanghai',
