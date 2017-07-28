a639a84f07bfcafd7ac9882278731fc47872b330
门殿春 <190636148@qq.com>
2017-07-28 15:39:57 +0800

    后台首页图片

--- backend/views/layouts/content.php
+++ backend/views/layouts/content.php
@@ -3,37 +3,54 @@ use yii\widgets\Breadcrumbs;
 use dmstr\widgets\Alert;
 
 ?>
+<?php
+    if(Yii::$app->controller->id == 'site' && Yii::$app->controller->action->id == 'index'){
+?>
+
 <div class="content-wrapper">
-    <section class="content-header">
-        <?php if (isset($this->blocks['content-header'])) { ?>
-            <h1><?= $this->blocks['content-header'] ?></h1>
-        <?php } else { ?>
-            <h1>
-                <?php
-                if ($this->title !== null) {
-                    echo \yii\helpers\Html::encode($this->title);
-                } else {
-                    echo \yii\helpers\Inflector::camel2words(
-                        \yii\helpers\Inflector::id2camel($this->context->module->id)
-                    );
-                    echo ($this->context->module->id !== \Yii::$app->id) ? '<small>Module</small>' : '';
-                } ?>
-            </h1>
-        <?php } ?>
+        <img width="100%" height="100%" src="images/index.jpeg" />
+    </div>
 
-        <?=
-        Breadcrumbs::widget(
-            [
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]
-        ) ?>
-    </section>
+        <?php
+    }else {
 
-    <section class="content">
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </section>
-</div>
+        ?>
+
+        <div class="content-wrapper">
+            <section class="content-header">
+                <?php if (isset($this->blocks['content-header'])) { ?>
+                    <h1><?= $this->blocks['content-header'] ?></h1>
+                <?php } else { ?>
+                    <h1>
+                        <?php
+                        if ($this->title !== null) {
+                            echo \yii\helpers\Html::encode($this->title);
+                        } else {
+                            echo \yii\helpers\Inflector::camel2words(
+                                \yii\helpers\Inflector::id2camel($this->context->module->id)
+                            );
+                            echo ($this->context->module->id !== \Yii::$app->id) ? '<small>Module</small>' : '';
+                        } ?>
+                    </h1>
+                <?php } ?>
+
+                <?=
+                Breadcrumbs::widget(
+                    [
+                        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                    ]
+                ) ?>
+            </section>
+
+            <section class="content">
+                <?= Alert::widget() ?>
+                <?= $content ?>
+            </section>
+        </div>
+
+<?php
+    }
+?>
 
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
