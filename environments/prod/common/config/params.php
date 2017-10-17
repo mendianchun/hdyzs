6320b5ec7b1724fb2b3fc6f19614547d043a5259
门殿春 <190636148@qq.com>
2017-10-17 14:14:54 +0800

    修改图片地址

--- environments/prod/common/config/params.php
+++ environments/prod/common/config/params.php
@@ -7,7 +7,7 @@ return [
 	'appointment.start_msg'=>'您好，您的会诊将于半小时后开始，请您准备',
 	'verifycode.msg' => '%s（动态验证码）,请在30分钟内填写。',
     // 图片服务器的域名设置，拼接保存在数据库中的相对地址，可通过web进行展示
-    'domain' => 'http://img.handianzy.com/',
+    'domain' => 'http://img.handianzy.com:8090/',
 	'score.key'=>'hhddyyzzss',
 
 	'time.8'=>'morning',
